556b26a2cff1ab124c859f80ee0528104cdbc46d
Renate Pyhel <Renate.Pyhel@staples.com>
2014-09-08 10:07:57 -0400

    Final pre-Kendall

--- app/src/main/java/com/staples/drawertest/MainActivity.java
+++ app/src/main/java/com/staples/drawertest/MainActivity.java
@@ -7,6 +7,9 @@ import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.widget.DrawerLayout;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
@@ -18,7 +21,7 @@ import android.widget.SearchView;
 /**
  * Created by PyhRe001 on 8/11/14.
  */
-public class MainActivity extends Activity implements View.OnClickListener, AdapterView.OnItemClickListener, SearchView.OnQueryTextListener {
+public class MainActivity extends Activity implements AdapterView.OnItemClickListener, SearchView.OnQueryTextListener {
 
     private static final String TAG = "MainActivity";
 
@@ -41,19 +44,23 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
 
         // Initialize action bar
         ActionBar actionBar = getActionBar();
-        actionBar.setDisplayHomeAsUpEnabled(false);
-        actionBar.setDisplayShowHomeEnabled(false);
-        actionBar.setDisplayShowCustomEnabled(true);
-        actionBar.setDisplayShowTitleEnabled(false);
+        actionBar.setLogo(R.drawable.ic_drawer);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setTitle(R.string.staples);
 
-        actionBar.setCustomView(R.layout.action_bar);
-        View view = actionBar.getCustomView();
-        view.findViewById(R.id.action_left_drawer).setOnClickListener(this);
-        view.findViewById(R.id.action_right_drawer).setOnClickListener(this);
-
-        searchText = (SearchView) findViewById(R.id.search_text);
-        searchText.setOnSearchClickListener(this);
-        searchText.setOnQueryTextListener(this);
+//        actionBar.setDisplayHomeAsUpEnabled(false);
+//        actionBar.setDisplayShowHomeEnabled(false);
+//        actionBar.setDisplayShowCustomEnabled(true);
+//        actionBar.setDisplayShowTitleEnabled(false);
+//
+//        actionBar.setCustomView(R.layout.action_bar);
+//        View view = actionBar.getCustomView();
+//        view.findViewById(R.id.action_left_drawer).setOnClickListener(this);
+//        view.findViewById(R.id.action_right_drawer).setOnClickListener(this);
+//
+//        searchText = (SearchView) findViewById(R.id.search_text);
+//        searchText.setOnSearchClickListener(this);
+//        searchText.setOnQueryTextListener(this);
 
         // Initialize left drawer listview
         DrawerAdapter adapter = new DrawerAdapter(this);
@@ -102,25 +109,32 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
     }
 
     private void closeKeyboard() {
-        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(searchText.getWindowToken(), 0);
+//        InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+//        imm.hideSoftInputFromWindow(searchText.getWindowToken(), 0);
     }
 
-    // ActionBar clicks
     @Override
-    public void onClick(View view) {
-        switch (view.getId()) {
-            case R.id.action_left_drawer:
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu items for use in the action bar
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.actions, menu);
+        return(super.onCreateOptionsMenu(menu));
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle presses on the action bar items
+        switch (item.getItemId()) {
+            case android.R.id.home:
                 closeKeyboard();
                 if (!drawerLayout.isDrawerOpen(leftDrawer)) {
                     drawerLayout.closeDrawer(rightDrawer);
                     drawerLayout.openDrawer(leftDrawer);
                 } else drawerLayout.closeDrawers();
-                break;
+                return(true);
 
-            case R.id.search_text:
-                drawerLayout.closeDrawers();
-                break;
+//            case R.id.search_text:
+//                drawerLayout.closeDrawers();
+//                return(true);
 
             case R.id.action_right_drawer:
                 closeKeyboard();
@@ -128,7 +142,10 @@ public class MainActivity extends Activity implements View.OnClickListener, Adap
                     drawerLayout.closeDrawer(leftDrawer);
                     drawerLayout.openDrawer(rightDrawer);
                 } else drawerLayout.closeDrawers();
-                break;
+                return(true);
+
+            default:
+                return super.onOptionsItemSelected(item);
         }
     }
 
