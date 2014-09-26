12be69c89cad04185682ceeb1ac45b3a850f9790
Oral, Burc <burcoral@alum.mit.edu>
2014-09-26 00:55:02 -0400

    changed LMS server URL

--- app/src/main/java/com/staples/mobile/common/access/lms/api/LmsApi.java
+++ app/src/main/java/com/staples/mobile/common/access/lms/api/LmsApi.java
@@ -8,7 +8,9 @@ import retrofit.http.GET;
 
 public interface LmsApi {
 //    public static final String SERVICE_ENDPOINT = "http://10.4.128.46:3000"; // node.js
-    public static final String SERVICE_ENDPOINT = "http://66.30.112.203:25624"; // rpi
+    //public static final String SERVICE_ENDPOINT = "http://66.30.112.203:25624"; // rpi
+    public static final String SERVICE_ENDPOINT = "http://204.232.161.159"; //  rackspace
+
 //    public static final String SERVICE_ENDPOINT = "http://10.29.172.60:9100"; // The office printer!
 
     @GET("/{version}/{storeId}/lms")
