87516df39313a0bcb6c83d77145735f3ebd9c4a3
Khandelwal, Amitav <Amitav.Khandelwal@Staples.com>
2014-10-13 16:10:30 -0400

    Added Cart viewing model. Some fields are null, needs fixing

---
+++ app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/ViewCart.java
@@ -0,0 +1,55 @@
+
+package com.staples.mobile.common.access.easyopen.model.cart;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewCart {
+
+    private List<Cart> cart = new ArrayList<Cart>();
+    private String recordSetComplete;
+    private String recordSetCount;
+    private String recordSetStartNumber;
+    private String recordSetTotal;
+
+    public List<Cart> getCart() {
+        return cart;
+    }
+
+    public void setCart(List<Cart> cart) {
+        this.cart = cart;
+    }
+
+    public String getRecordSetComplete() {
+        return recordSetComplete;
+    }
+
+    public void setRecordSetComplete(String recordSetComplete) {
+        this.recordSetComplete = recordSetComplete;
+    }
+
+    public String getRecordSetCount() {
+        return recordSetCount;
+    }
+
+    public void setRecordSetCount(String recordSetCount) {
+        this.recordSetCount = recordSetCount;
+    }
+
+    public String getRecordSetStartNumber() {
+        return recordSetStartNumber;
+    }
+
+    public void setRecordSetStartNumber(String recordSetStartNumber) {
+        this.recordSetStartNumber = recordSetStartNumber;
+    }
+
+    public String getRecordSetTotal() {
+        return recordSetTotal;
+    }
+
+    public void setRecordSetTotal(String recordSetTotal) {
+        this.recordSetTotal = recordSetTotal;
+    }
+
+}
