4b0d8ba347c60a0ce58a88be324af21de92f3917
Sutlief, Diana <Diana.Sutlief@Staples.com>
2014-10-17 10:39:10 -0700

    adding message field (e.g. this gets filled in when item out of stock)

--- app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/CartUpdate.java
+++ app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/CartUpdate.java
@@ -1,10 +1,15 @@
 
 package com.staples.mobile.common.access.easyopen.model.cart;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class CartUpdate {
+
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private String message;
 
     private List<ItemsAdded> itemsAdded = new ArrayList<ItemsAdded>();
 
@@ -14,6 +19,13 @@ public class CartUpdate {
 
     public void setItemsAdded(List<ItemsAdded> itemsAdded) {
         this.itemsAdded = itemsAdded;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+    public void setMessage(String message) {
+        this.message = message;
     }
 
 }
