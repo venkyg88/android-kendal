21e75e82ada0c7c8419591f419a4588ef286f8cd
Dow, Stephen <Stephen.Dow@Staples.com>
2014-10-29 15:40:50 -0400

    US-105-LMS. Checkpoint.

--- app/src/main/java/com/staples/mobile/common/access/lms/api/LmsApi.java
+++ app/src/main/java/com/staples/mobile/common/access/lms/api/LmsApi.java
@@ -1,22 +1,22 @@
 package com.staples.mobile.common.access.lms.api;
 
-import com.staples.mobile.common.access.lms.model.Lms;
+import com.staples.mobile.common.access.configurator.model.Configurator;
 
 import retrofit.Callback;
 import retrofit.http.EncodedPath;
 import retrofit.http.GET;
 
 public interface LmsApi {
-//    public static final String INSECURE_ENDPOINT = "http://10.4.128.46:3000"; // node.js
-//    public static final String INSECURE_ENDPOINT = "http://66.30.112.203:25624"; // rpi
+    // public static final String INSECURE_ENDPOINT = "http://10.4.128.46:3000"; // node.js
+    // public static final String INSECURE_ENDPOINT = "http://66.30.112.203:25624"; // rpi
     public static final String INSECURE_ENDPOINT = "http://204.232.161.159"; //  rackspace
 
-//    public static final String SERVICE_ENDPOINT = "http://10.29.172.60:9100"; // The office printer!
+    // public static final String SERVICE_ENDPOINT = "http://10.29.172.60:9100"; // The office printer!
 
     @GET("/{version}/{storeId}/lms")
     void lms(
-        @EncodedPath("version") String version,
-        @EncodedPath("storeId") String storeId,
-        Callback<Lms> callback
+            @EncodedPath("version") String version,
+            @EncodedPath("storeId") String storeId,
+            Callback<Configurator> callback
     );
 }
