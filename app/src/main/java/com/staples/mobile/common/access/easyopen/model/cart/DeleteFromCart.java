d810fcdbfc086587a5422d2f62e56684ec8282a4
Sutlief, Diana <Diana.Sutlief@Staples.com>
2014-10-31 15:26:11 -0700

    adding List<ApiError> errors to DeleteFromCart.java

--- app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/DeleteFromCart.java
+++ app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/DeleteFromCart.java
@@ -1,7 +1,18 @@
 package com.staples.mobile.common.access.easyopen.model.cart;
 
 
+import com.staples.mobile.common.access.easyopen.model.ApiError;
+
+import java.util.List;
+
 //placeholder class for delete from cart responses. Since a successful deletion is
 //supposed to give an empty response, this has been left blank.
 public class DeleteFromCart {
+
+    // include this so that ((CartContents)retrofitError.getBody()).getErrors()
+    // can be examined in 400 Bad Request failure response (e.g. in debugger)
+    private List<ApiError> errors;
+    public List<ApiError> getErrors() { return errors; }
+    public void setErrors(List<ApiError> errors) { this.errors = errors; }
+
 }
