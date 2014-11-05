70ac9709fa69a90a28d8f4692f1f274df43a175f
Sutlief, Diana <Diana.Sutlief@Staples.com>
2014-11-04 17:45:26 -0800

    deleting incorrect comment

--- app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/PaymentMethodResponse.java
+++ app/src/main/java/com/staples/mobile/common/access/easyopen/model/cart/PaymentMethodResponse.java
@@ -10,10 +10,8 @@ import com.staples.mobile.common.access.easyopen.model.SupportsApiErrors;
 
 import java.util.List;
 
-//placeholder class for delete from cart responses. Since a successful deletion is
-//supposed to give an empty response, this has been left blank.
 public class PaymentMethodResponse implements SupportsApiErrors {
-
+ed
     // include this so that ((CartContents)retrofitError.getBody()).getErrors()
     // can be examined in 400 Bad Request failure response (e.g. in debugger)
     private List<ApiError> errors;
