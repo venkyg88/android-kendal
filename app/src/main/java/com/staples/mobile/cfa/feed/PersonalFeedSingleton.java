95e38bd870d7ed13b8bbf5763c370ad6e98887df
Yongnan Zhou <yongnan.zhou@staples.com>
2014-11-18 15:13:25 -0500

    add personal feed singleton

---
+++ app/src/main/java/com/staples/mobile/cfa/feed/PersonalFeedSingleton.java
@@ -0,0 +1,92 @@
+package com.staples.mobile.cfa.feed;
+
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.util.HashSet;
+
+/**
+ * Author: Yongnan Zhou
+ */
+
+public class PersonalFeedSingleton {
+    private static final String TAG = "PersonalFeedData";
+    private static final int SEEN_PRODUCTS_AMOUNT = 3;
+
+    private static PersonalFeedSingleton personalFeedSingleton = null;
+    private SizedArrayList<SeenProductsRowItem> savedSeenProducts = null;
+    private HashSet<String> savedSkus = null;
+
+    private PersonalFeedSingleton(int size){
+        savedSeenProducts = new SizedArrayList<SeenProductsRowItem>(size);
+        savedSkus = new HashSet<String>();
+    }
+
+    public static PersonalFeedSingleton getInstance( ) {
+        if(personalFeedSingleton == null){
+            personalFeedSingleton = new PersonalFeedSingleton(SEEN_PRODUCTS_AMOUNT);
+        }
+        return personalFeedSingleton;
+    }
+
+    // get seen products not persistently
+    public SizedArrayList<SeenProductsRowItem> getSavedSeenProducts(){
+        return savedSeenProducts;
+    }
+
+    // get seen products not persistently
+    public SizedArrayList<SeenProductsRowItem> getSavedSeenProducts(Activity activity) {
+        // Get saved seen products info from the phone
+        SharedPreferences sp = activity.getSharedPreferences("SAVED_SEEN_PRODUCTS", activity.MODE_PRIVATE);
+        String savedProductsString = sp.getString("SEEN_PRODUCT_LIST", "");
+
+        final String FIELD_SEPARATOR = "/_-_/";
+        final String OBJECT_SEPARATOR = "/_&_/";
+
+        // initialize SeenProductsRowItem list
+        SizedArrayList<SeenProductsRowItem> savedProductsList = new SizedArrayList<SeenProductsRowItem>(3);;
+
+        if (!savedProductsString.equals("")) {
+            SeenProductsRowItem savedSeenProduct;
+            String[] savedProductsArray = savedProductsString.split(OBJECT_SEPARATOR);
+
+            for (int i = 0; i < savedProductsArray.length; i++) {
+                String savedProductString = savedProductsArray[i];
+                String[] productFields = savedProductString.split(FIELD_SEPARATOR);
+
+                // safe check for empty fields
+                if(productFields.length > 1) {
+                    String sku = productFields[0];
+                    String productName = productFields[1];
+                    String currentPrice = productFields[2];
+                    String reviewCount = productFields[3];
+                    String rating = productFields[4];
+                    String unitOfMeasure = productFields[5];
+                    String imageUrl = productFields[6];
+
+                    savedSeenProduct = new SeenProductsRowItem(sku, productName, currentPrice, reviewCount,
+                            rating, unitOfMeasure, imageUrl);
+
+                    savedProductsList.add(savedSeenProduct);
+                    Log.d(TAG, i + "th Saved Seen Product -> " + savedProductString);
+                }
+                else{
+                    Log.d(TAG, "No Saved Seen Products Found!");
+                }
+            }
+        }
+
+       return savedProductsList;
+    }
+
+    // get seen products' sku not persistently
+    public HashSet<String> getSavedSku(){
+        return savedSkus;
+    }
+
+    // get seen products' sku persistently
+    public HashSet<String> getSavedSku(Activity activity) {
+        return savedSkus;
+    }
+}
