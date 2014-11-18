7c45e251631bd40ae7543886d91c2eb041563d29
Yongnan Zhou <yongnan.zhou@staples.com>
2014-11-18 12:16:56 -0500

    add getSavedSeenProducts persistently

--- app/src/main/java/com/staples/mobile/cfa/feed/PersonalFeedData.java
+++ app/src/main/java/com/staples/mobile/cfa/feed/PersonalFeedData.java
@@ -1,5 +1,10 @@
 package com.staples.mobile.cfa.feed;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
+
+import java.util.ArrayList;
 import java.util.HashSet;
 
 /**
@@ -7,6 +12,7 @@ import java.util.HashSet;
  */
 
 public class PersonalFeedData {
+    private static final String TAG = "PersonalFeedData";
     private static PersonalFeedData singleton = null;
     private static final int SEEN_PRODUCTS_AMOUNT = 3;
     private SizedArrayList<SeenProductsRowItem> savedSeenProducts = null;
@@ -26,6 +32,28 @@ public class PersonalFeedData {
 
     public SizedArrayList<SeenProductsRowItem> getSavedSeenProducts(){
         return savedSeenProducts;
+    }
+
+    public void getSavedSeenProducts(Activity activity) {
+        SharedPreferences sp = activity.getSharedPreferences("SAVED_SEEN_PRODUCTS", activity.MODE_PRIVATE);
+
+        String savedSkuString = sp.getString("SKU_LIST", "");
+        String savedProductString = sp.getString("SEEN_PRODUCT_LIST", "");
+
+        final String FIELD_SEPERATOR = "/_/";
+        final String OBJECT_SEPERATOR = "/&/";
+
+        if (!savedProductString.equals("")) {
+            String[] savedProductsArray = savedProductString.split(OBJECT_SEPERATOR);
+
+            SizedArrayList<SeenProductsRowItem> savedProductsList = new SizedArrayList<SeenProductsRowItem>(3);
+            for (int i = 0; i < savedProductsArray.length; i++) {
+                Log.d(TAG, "Each Saved Product -> " + savedProductsArray[i]);
+                //savedProductsList.add(savedProductsArray[i]);
+            }
+        }
+
+       //return savedProductsList;
     }
 
     public HashSet<String> getSavedSku(){
