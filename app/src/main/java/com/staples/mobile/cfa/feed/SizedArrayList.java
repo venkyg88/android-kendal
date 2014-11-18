e24371b960b0a1d0fcb879eee6153d644cd3dd52
Yongnan Zhou <yongnan.zhou@staples.com>
2014-11-18 16:47:01 -0500

    Save seen products persistently on phone

--- app/src/main/java/com/staples/mobile/cfa/feed/SizedArrayList.java
+++ app/src/main/java/com/staples/mobile/cfa/feed/SizedArrayList.java
@@ -1,5 +1,9 @@
 package com.staples.mobile.cfa.feed;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
+import android.util.Log;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 
@@ -8,6 +12,7 @@ import java.util.HashSet;
  */
 
 public class SizedArrayList<T> extends ArrayList<T> {
+    private static final String TAG = "SizedArrayList";
     private int maxSize;
 
     public SizedArrayList(int size) {
@@ -15,10 +20,12 @@ public class SizedArrayList<T> extends ArrayList<T> {
         this.maxSize = size;
     }
 
+    // save seen product not persistently
     public boolean addSeenProduct(T object, String sku){
         boolean isDone = super.add(object);
-        PersonalFeedData feedSingleton = PersonalFeedData.getInstance();
-        HashSet<String> savedSkus = feedSingleton.getSavedSku();
+
+        PersonalFeedSingleton feedSingleton = PersonalFeedSingleton.getInstance();
+        HashSet<String> savedSkus = feedSingleton.getSavedSkus();
 
         // Remove elements until it's the right size.
         if (size() > maxSize){
@@ -30,5 +37,71 @@ public class SizedArrayList<T> extends ArrayList<T> {
 
         savedSkus.add(sku);
         return isDone;
+    }
+
+    // save seen product persistently
+    public boolean addSeenProduct(T object, String sku, Activity activity){
+        boolean isDone = super.add(object);
+        SeenProductsRowItem seenProduct = (SeenProductsRowItem) object;
+
+        saveSeenProductInPhone(seenProduct, activity);
+
+        PersonalFeedSingleton feedSingleton = PersonalFeedSingleton.getInstance();
+        HashSet<String> savedSkus = feedSingleton.getSavedSkus(activity);
+
+        // Remove elements until it's the right size.
+        if (size() > maxSize){
+            // remove the earliest saved sku
+            savedSkus.remove(sku);
+            // remove the earliest saved product
+            this.remove(0);
+
+            //saveLastSeenProductInPhone(this);
+        }
+
+        savedSkus.add(sku);
+        return isDone;
+    }
+
+    private void saveSeenProductInPhone(SeenProductsRowItem seenProduct, Activity activity){
+        SharedPreferences sp = activity.getSharedPreferences("SAVED_SEEN_PRODUCTS", activity.MODE_PRIVATE);
+
+        final String FIELD_SEPARATOR = "/_-_/";
+        final String OBJECT_SEPARATOR = "/_&_/";
+        String savedSkuString = sp.getString("SEEN_PRODUCT_SKU_LIST", "");
+        String savedProductString = sp.getString("SEEN_PRODUCT_LIST", "");
+
+        // first time
+        if(savedSkuString.equals("") && savedProductString.equals("")){
+            savedSkuString = seenProduct.getSku();
+
+            savedProductString = seenProduct.getSku() + FIELD_SEPARATOR
+                    + seenProduct.getProduceName() + FIELD_SEPARATOR
+                    + seenProduct.getCurrentPrice() + FIELD_SEPARATOR
+                    + seenProduct.getReviewCount() + FIELD_SEPARATOR
+                    + seenProduct.getRating() + FIELD_SEPARATOR
+                    + seenProduct.getUnitOfMeasure() + FIELD_SEPARATOR
+                    + seenProduct.getImageUrl();
+        }
+        else{
+            savedSkuString = savedSkuString + FIELD_SEPARATOR + seenProduct.getSku();
+
+            String productString = seenProduct.getSku() + FIELD_SEPARATOR
+                    + seenProduct.getProduceName() + FIELD_SEPARATOR
+                    + seenProduct.getCurrentPrice() + FIELD_SEPARATOR
+                    + seenProduct.getReviewCount() + FIELD_SEPARATOR
+                    + seenProduct.getRating() + FIELD_SEPARATOR
+                    + seenProduct.getUnitOfMeasure() + FIELD_SEPARATOR
+                    + seenProduct.getImageUrl();
+            savedProductString = savedProductString + OBJECT_SEPARATOR + productString;
+        }
+
+        // save updated data
+        SharedPreferences.Editor editor = sp.edit();
+        editor.putString("SEEN_PRODUCT_SKU_LIST", savedSkuString);
+        editor.putString("SEEN_PRODUCT_LIST", savedProductString);
+        editor.commit();
+
+        Log.d(TAG, "Saved seen products successfully! -> " + savedProductString);
     }
 }
