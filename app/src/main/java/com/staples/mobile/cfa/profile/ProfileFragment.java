2d5440a9b7d74812b3a612b035aee03e9073ac3c
Dodda, Avinash <Avinash.Dodda@Staples.com>
2014-11-21 00:01:54 -0500

    split list fragment into two classes to deal with independent objects and their properties

--- app/src/main/java/com/staples/mobile/cfa/profile/ProfileFragment.java
+++ app/src/main/java/com/staples/mobile/cfa/profile/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.staples.mobile.cfa.profile;
 
+import android.app.Activity;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.util.Log;
@@ -7,24 +8,18 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.TextView;
 
 import com.staples.mobile.R;
 import com.staples.mobile.cfa.MainActivity;
 import com.staples.mobile.cfa.login.LoginHelper;
 import com.staples.mobile.cfa.widget.LinearLayoutWithProgressOverlay;
-import com.staples.mobile.common.access.Access;
 import com.staples.mobile.common.access.easyopen.api.EasyOpenApi;
 import com.staples.mobile.common.access.easyopen.model.member.*;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
-
-import retrofit.Callback;
-import retrofit.RetrofitError;
-import retrofit.client.Response;
 
 public class ProfileFragment extends Fragment implements ProfileDetails.ProfileRefreshCallback, View.OnClickListener{
     private static final String TAG = "ProfileFragment";
@@ -38,11 +33,13 @@ public class ProfileFragment extends Fragment implements ProfileDetails.ProfileR
     Button shippingBtn;
     Button ccBtn;
     private LinearLayoutWithProgressOverlay profileLayout;
+    Activity activity;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
 
         Log.d(TAG, "onCreateView()");
+        activity = getActivity();
         View view = inflater.inflate(R.layout.profile_fragment, container, false);
         profileLayout = (LinearLayoutWithProgressOverlay) view.findViewById(R.id.profile_fragment_content);
         profileLayout.setCartProgressOverlay(view.findViewById(R.id.profile_progress_overlay));
@@ -124,56 +121,26 @@ public class ProfileFragment extends Fragment implements ProfileDetails.ProfileR
         switch(view.getId()) {
             case R.id.addShippingBtn:
                 if(ProfileDetails.hasAddress()) {
-                    ArrayList<String> addressList = new ArrayList<String>();
-                    for(Iterator<Address> addresses = ProfileDetails.getMember().getAddress().iterator(); addresses.hasNext();) {
-                        Address address = addresses.next();
-                        String tmpAddress = address.getFirstname() + "," + address.getLastname() + "," + "\n" +
-                                address.getAddress1() + "," + "\n" +
-                                address.getCity() + ", " + address.getState() + " " + address.getZipcode() + "\n" +
-                                address.getPhone1();
-                        addressList.add(tmpAddress);
-                    }
-
-                    Fragment listFragment = Fragment.instantiate(getActivity(), ListFragment.class.getName());
-                    Bundle bundle = new Bundle();
-                    bundle.putStringArrayList("addresses",addressList);
-                    listFragment.setArguments(bundle);
-                    ((MainActivity)getActivity()).navigateToFragment(listFragment);
+                    Fragment shippingListFragment = Fragment.instantiate(activity, ShippingListFragment.class.getName());
+                    ((MainActivity) activity).navigateToFragment(shippingListFragment);
                     break;
                 }
                 else{
-                    Fragment shippingFragment = Fragment.instantiate(getActivity(), ShippingFragment.class.getName());
-                    ((MainActivity) getActivity()).navigateToFragment(shippingFragment);
+                    Fragment shippingFragment = Fragment.instantiate(activity, ShippingFragment.class.getName());
+                    ((MainActivity) activity).navigateToFragment(shippingFragment);
                     break;
                 }
 
 
             case R.id.addCCBtn:
                 if(ProfileDetails.hasPaymentMethod()) {
-                    ArrayList<String> cardList = new ArrayList<String>();
-                    for (Iterator<CCDetails> cards = ProfileDetails.getMember().getCreditCard().iterator(); cards.hasNext(); ) {
-                        CCDetails cardDetail = cards.next();
-                        String cardNumber;
-                        if (cardDetail.getCardNumber().length() > 4) {
-                            cardNumber = cardDetail.getCardNumber().substring(cardDetail.getCardNumber().length() - 4);
-                        } else {
-                            cardNumber = cardDetail.getCardNumber();
-                        }
-                        String tmpCard = "Card ending in " + cardNumber + "\n" +
-                                cardDetail.getCardType() + "\n" +
-                                "Exp. " + cardDetail.getExpirationMonth() + "/" + cardDetail.getExpirationYear();
-                        cardList.add(tmpCard);
-                    }
-                    Fragment listFragment = Fragment.instantiate(getActivity(), ListFragment.class.getName());
-                    Bundle bundle = new Bundle();
-                    bundle.putStringArrayList("cards", cardList);
-                    listFragment.setArguments(bundle);
-                    ((MainActivity) getActivity()).navigateToFragment(listFragment);
+                    Fragment cardListFragment = Fragment.instantiate(activity, CreditCardListFragment.class.getName());
+                    ((MainActivity) activity).navigateToFragment(cardListFragment);
                     break;
                 }
                 else{
-                    Fragment cardFragment = Fragment.instantiate(getActivity(), CreditCardFragment.class.getName());
-                    ((MainActivity) getActivity()).navigateToFragment(cardFragment);
+                    Fragment cardFragment = Fragment.instantiate(activity, CreditCardFragment.class.getName());
+                    ((MainActivity) activity).navigateToFragment(cardFragment);
                     break;
                 }
         }
