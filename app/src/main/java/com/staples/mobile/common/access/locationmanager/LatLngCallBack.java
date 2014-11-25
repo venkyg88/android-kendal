f31ff01c27409d950d21ea9139d5fd82c5080c0e
parve002 <venkata.parchuri@staples.com>
2014-11-25 08:42:26 -0500

    Added two service classes for proving location Latitude & Longitude or ZipCode.

--- app/src/main/java/com/staples/mobile/common/access/locationmanager/LatLngCallBack.java
+++
@@ -1,7 +0,0 @@
-package com.staples.mobile.common.access.locationmanager;
-
-import com.google.android.gms.maps.model.LatLng;
-
-public interface LatLngCallBack {
-    void userLatLng(LatLng latLng);
-}
--- app/src/main/java/com/staples/mobile/common/access/locationmanager/ZipCodeCallBack.java
+++
@@ -1,5 +0,0 @@
-package com.staples.mobile.common.access.locationmanager;
-
-public interface ZipCodeCallBack{
-    void userZipCode(String zipCode);
-}
