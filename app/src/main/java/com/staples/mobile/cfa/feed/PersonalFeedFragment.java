bd8b528091d536b42b37c03d5d3c38e0bb8feeae
Yongnan Zhou <yongnan.zhou@staples.com>
2014-11-26 13:30:24 -0500

    check if empty

--- app/src/main/java/com/staples/mobile/cfa/feed/PersonalFeedFragment.java
+++ app/src/main/java/com/staples/mobile/cfa/feed/PersonalFeedFragment.java
@@ -14,7 +14,7 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
-import com.staples.mobile.R;
+import com.staples.mobile.cfa.R;
 import com.staples.mobile.cfa.BaseFragment;
 import com.staples.mobile.cfa.MainActivity;
 import com.staples.mobile.cfa.cart.CartFragment;
@@ -23,23 +23,25 @@ import com.staples.mobile.cfa.widget.PriceSticker;
 import com.staples.mobile.cfa.widget.RatingStars;
 import com.staples.mobile.common.access.easyopen.model.browse.Product;
 
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 
 public class PersonalFeedFragment extends BaseFragment
         implements DailyDealProductCollection.ProductCollectionCallBack,
-                   ClearanceProductCollection.ProductCollectionCallBack {
+                   ClearanceProductCollection.ProductCollectionCallBack{
     private static final String TAG = "PersonalFeedFragment";
 
-    private static final String DAILY_DEAL_IDENTIFIER = "BI739472";
-    private static final String CLEARANCE_IDENTIFIER = "BI642994";
+    public static final String DAILY_DEAL_IDENTIFIER = "BI739472";
+    public static final String CLEARANCE_IDENTIFIER = "BI642994";
 
     private LinearLayout clearanceContainer;
     private LinearLayout dailyDealContainer;
     private LinearLayout seenProductsContainer;
 
     private TextView clearanceProductsNotFound;
-    private TextView dailyDealsenProductsNotFound;
+    private TextView dailyDealProductsNotFound;
     private TextView seenProductsNotFound;
 
     @Override
@@ -51,12 +53,17 @@ public class PersonalFeedFragment extends BaseFragment
         seenProductsContainer = (LinearLayout) personalFeedLayout.findViewById(R.id.seen_products_container);
 
         clearanceProductsNotFound = (TextView) personalFeedLayout.findViewById(R.id.clearance_empty);
-        dailyDealsenProductsNotFound = (TextView) personalFeedLayout.findViewById(R.id.daily_deal_empty);
+        dailyDealProductsNotFound = (TextView) personalFeedLayout.findViewById(R.id.daily_deal_empty);
         seenProductsNotFound = (TextView) personalFeedLayout.findViewById(R.id.seen_products_empty);
 
+        clearanceProductsNotFound.setVisibility(View.VISIBLE);
+        dailyDealProductsNotFound.setVisibility(View.VISIBLE);
+
+        // make daily deal api call
         DailyDealProductCollection dailyDealProductCollection = new DailyDealProductCollection();
         dailyDealProductCollection.getProducts(DAILY_DEAL_IDENTIFIER, 1, 50, this); // identifier, offset, limit, callback
 
+        // make clearance api call
         ClearanceProductCollection clearanceProductCollection = new ClearanceProductCollection();
         clearanceProductCollection.getProducts(CLEARANCE_IDENTIFIER, 1, 50, this); // identifier, offset, limit, callback
 
@@ -119,11 +126,12 @@ public class PersonalFeedFragment extends BaseFragment
     }
 
     @Override
-    public void onProductCollectionResult(List<Product> dailyDealProducts) {
+    public void onDailyDealProductResult(List<Product> dailyDealProducts) {
         HashSet<String> dailyDealSkuSet = new HashSet<String>();
         for(Product p: dailyDealProducts){
             dailyDealSkuSet.add(p.getSku());
-            Log.d(TAG, "Daily Deal Products: " + p.getProductName() + "-" + p.getSku());
+            //fillContainer(p, dailyDealContainer);
+            //Log.d(TAG, "Daily Deal Products: " + p.getProductName() + "-" + p.getSku());
         }
 
         List<CartItem> cartItems = CartFragment.getListItems();
@@ -137,19 +145,20 @@ public class PersonalFeedFragment extends BaseFragment
 
         // display "nothing found" if no daily deal products
         if(dailyDealContainer.getChildCount() == 0){
-            dailyDealsenProductsNotFound.setVisibility(View.VISIBLE);
+            dailyDealProductsNotFound.setVisibility(View.VISIBLE);
         }
         else{
-            dailyDealsenProductsNotFound.setVisibility(View.GONE);
+            dailyDealProductsNotFound.setVisibility(View.GONE);
         }
     }
 
     @Override
-    public void onClearanceProductsResult(List<Product> clearanceProducts) {
+    public void onClearanceProductsResult(List<Product> clearanceProducts){
         HashSet<String> clearanceSkuSet = new HashSet<String>();
         for(Product p: clearanceProducts){
             clearanceSkuSet.add(p.getSku());
-            Log.d(TAG, "Clearance Products: " + p.getProductName() + "-" + p.getSku());
+            //fillContainer(p, clearanceContainer);
+            //Log.d(TAG, "Clearance Products: " + p.getProductName() + "-" + p.getSku());
         }
 
         List<CartItem> cartItems = CartFragment.getListItems();
@@ -198,6 +207,49 @@ public class PersonalFeedFragment extends BaseFragment
 
         // Set listener for all the items
         final String sku = cartItem.getProduct().getSku();
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).selectSkuItem(sku);
+            }
+        });
+
+        title.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ((MainActivity) getActivity()).selectSkuItem(sku);
+            }
+        });
+    }
+
+    private void fillContainer(Product cartItemProduct, LinearLayout container){
+        String productName = cartItemProduct.getProductName();
+        String currentPrice = String.valueOf(cartItemProduct.getPricing().get(0).getFinalPrice());
+        String reviewCount = String.valueOf(cartItemProduct.getCustomerReviewCount());
+        String rating = String.valueOf(cartItemProduct.getCustomerReviewRating());
+        String unitOfMeasure = cartItemProduct.getPricing().get(0).getUnitOfMeasure();
+        String imageUrl = cartItemProduct.getImage().get(0).getUrl();
+
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        View row = inflater.inflate(R.layout.personal_feed_product_item, null);
+
+        TextView title = (TextView) row.findViewById(R.id.title);
+        title.setText(productName);
+
+        RatingStars ratingStars = (RatingStars) row.findViewById(R.id.rating);
+        ratingStars.setRating(Float.parseFloat(rating),
+                Integer.parseInt(reviewCount));
+
+        PriceSticker priceSticker = (PriceSticker) row.findViewById(R.id.pricing);
+        priceSticker.setPricing(Float.parseFloat(currentPrice), unitOfMeasure);
+
+        ImageView imageView = (ImageView) row.findViewById(R.id.image);
+        Picasso.with(getActivity()).load(imageUrl).error(R.drawable.no_photo).into(imageView);
+
+        container.addView(row);
+
+        // Set listener for all the items
+        final String sku = cartItemProduct.getSku();
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
