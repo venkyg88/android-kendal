6ecda938b8179cce5eaca55d7881af44a292b49c
Sutlief, Diana <Diana.Sutlief@Staples.com>
2014-12-18 18:30:46 -0800

    changing cart to use CardView

--- app/src/main/java/com/staples/mobile/cfa/cart/CartItem.java
+++ app/src/main/java/com/staples/mobile/cfa/cart/CartItem.java
@@ -13,12 +13,12 @@ import com.staples.mobile.common.access.easyopen.model.cart.Product;
 import java.util.List;
 
 public class CartItem {
-    private static final String TAG = "CartItem";
+    private static final String TAG = CartItem.class.getSimpleName();
 
     private Product product;
     private int proposedQty;
-    String expectedDelivery;
-    int expectedDeliveryItemQty;
+//    String expectedDelivery;
+//    int expectedDeliveryItemQty;
     int minExpectedBusinessDays;
     int maxExpectedBusinessDays;
 
@@ -111,24 +111,25 @@ public class CartItem {
         return null;
     }
 
-    public String getExpectedDelivery() {
-        return expectedDelivery;
-    }
-
-    public void setExpectedDelivery(String expectedDelivery) {
-        this.expectedDelivery = expectedDelivery;
-    }
-
-    public int getExpectedDeliveryItemQty() {
-        return expectedDeliveryItemQty;
-    }
-
-    public void setExpectedDeliveryItemQty(int expectedDeliveryItemQty) {
-        this.expectedDeliveryItemQty = expectedDeliveryItemQty;
-    }
+//    public String getExpectedDelivery() {
+//        return expectedDelivery;
+//    }
+//
+//    public void setExpectedDelivery(String expectedDelivery) {
+//        this.expectedDelivery = expectedDelivery;
+//    }
+//
+//    public int getExpectedDeliveryItemQty() {
+//        return expectedDeliveryItemQty;
+//    }
+//
+//    public void setExpectedDeliveryItemQty(int expectedDeliveryItemQty) {
+//        this.expectedDeliveryItemQty = expectedDeliveryItemQty;
+//    }
 
     public String getLeadTimeDescription() {
-        return product.getLeadTimeDescription();
+        // CartFragment logic requires this to be non null
+        return product.getLeadTimeDescription() == null? "" : product.getLeadTimeDescription();
     }
 
     public int getQuantity() {
@@ -154,14 +155,6 @@ public class CartItem {
     public void resetProposedQty() {
         setProposedQty(getQuantity());
     }
-
-//    public QuantityEditor getQtyWidget() {
-//        return qtyWidget;
-//    }
-//
-//    public void setQtyWidget(QuantityEditor qtyWidget) {
-//        this.qtyWidget = qtyWidget;
-//    }
 
     public QuantityEditor getQtyWidget() {
         return qtyWidget;
