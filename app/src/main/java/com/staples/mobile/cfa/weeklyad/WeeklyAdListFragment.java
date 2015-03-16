fd88c7fdb9172ad22006f27e0831c7c2d17527e0
Sutlief, Diana <Diana.Sutlief@Staples.com>
2015-03-16 11:22:53 -0700

    adding listingIndex and listingCount parameters to getWeeklyAdCategoryListing

--- app/src/main/java/com/staples/mobile/cfa/weeklyad/WeeklyAdListFragment.java
+++ app/src/main/java/com/staples/mobile/cfa/weeklyad/WeeklyAdListFragment.java
@@ -148,7 +148,8 @@ public class WeeklyAdListFragment extends Fragment implements View.OnClickListen
         activity.showProgressIndicator();
         int imageWidth = (int) activity.getResources().getDimension(R.dimen.weekly_ad_list_item_image_width);
         easyOpenApi = Access.getInstance().getEasyOpenApi(false);
-        easyOpenApi.getWeeklyAdCategoryListing(storeNumber, categoryTreeIds.get(currentTabIndex), imageWidth,
+        easyOpenApi.getWeeklyAdCategoryListing(storeNumber, categoryTreeIds.get(currentTabIndex),
+                imageWidth, 1, 100,
                 new Callback<WeeklyAd>() {
             @Override
             public void success(WeeklyAd weeklyAd, Response response) {
