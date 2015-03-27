4454d6599e73f3aac57ea93b10dcb9c576f8c26f
Yongnan Zhou <yongnan.zhou@staples.com>
2015-03-27 12:47:59 -0400

    mcs

--- app/src/dev/java/com/staples/mobile/cfa/FlavorSpecific.java
+++ app/src/dev/java/com/staples/mobile/cfa/FlavorSpecific.java
@@ -4,6 +4,6 @@ package com.staples.mobile.cfa;
 
 public class FlavorSpecific {
     // tapi for EasyOpen2, EasyOpen staies the same
-    public static final String MCS_URL = "http://minternal-preprod.staples.com/mobileServices/chapi/android/tag/CFA/dev/yongnan";
+    public static final String MCS_URL = "http://minternal-preprod.staples.com/mobileServices/chapi/android/tag/CFA/dev/latest";
     public static final String CRITTERCISM_ID = "54ff318de0697fa4496374fa";
 }
