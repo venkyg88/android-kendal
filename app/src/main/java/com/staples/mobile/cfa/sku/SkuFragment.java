b4a88023793383eaf24bc75d980fcc8e17273b5d
Sutlief, Diana <Diana.Sutlief@Staples.com>
2015-04-08 13:25:53 -0700

    analytics - fixing accessory action

--- app/src/main/java/com/staples/mobile/cfa/sku/SkuFragment.java
+++ app/src/main/java/com/staples/mobile/cfa/sku/SkuFragment.java
@@ -446,7 +446,7 @@ Callback,
             accessoryImageView.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    Tracker.getInstance().trackActionForProductAccessories(product); // analytics
+                    Tracker.getInstance().trackActionForProductAccessories(sku, product, false); // analytics
                     activity.selectSkuItem(accessoryTitle, sku, false);
                 }
             });
