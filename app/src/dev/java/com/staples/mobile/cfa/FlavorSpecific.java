1f7b36f2bab5a10bf71f2bc3bf966ac0e35e5c42
Renate Pyhel <Renate.Pyhel@staples.com>
2015-04-30 17:12:14 -0400

    Expiry notice

--- app/src/dev/java/com/staples/mobile/cfa/FlavorSpecific.java
+++ app/src/dev/java/com/staples/mobile/cfa/FlavorSpecific.java
@@ -3,6 +3,6 @@ package com.staples.mobile.cfa;
 // Development version
 
 public class FlavorSpecific {
-    public static final String MCS_URL = "http://minternal-dev.staples.com/mobileServices/chapi/android/tag/CFA/dev/latest";
+    public static final String MCS_URL = "http://minternal-dev.staples.com/mobileServices/chapi/mcs/tag/CFA/dev/1.0";
     public static final String CRITTERCISM_ID = "54ff318de0697fa4496374fa";
 }
--- app/src/qa/java/com/staples/mobile/cfa/FlavorSpecific.java
+++ app/src/qa/java/com/staples/mobile/cfa/FlavorSpecific.java
@@ -3,6 +3,6 @@ package com.staples.mobile.cfa;
 // Development version
 
 public class FlavorSpecific {
-    public static final String MCS_URL = "http://minternal-dev.staples.com/mobileServices/chapi/android/tag/CFA/qa/latest";
+    public static final String MCS_URL = "http://minternal-dev.staples.com/mobileServices/chapi/mcs/tag/CFA/qa/1.0";
     public static final String CRITTERCISM_ID = "5538e9208172e25e67906a3b";
 }
