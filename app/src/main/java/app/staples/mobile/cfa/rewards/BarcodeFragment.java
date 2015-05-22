3d113074c17b16aeb2a14c5525bf9ff6ec251abb
Kumar, Praween <Praween.Kumar@Staples.com>
2015-05-22 16:40:29 -0400

    Coupon barcode layout modified.

--- app/src/main/java/app/staples/mobile/cfa/rewards/BarcodeFragment.java
+++ app/src/main/java/app/staples/mobile/cfa/rewards/BarcodeFragment.java
@@ -18,14 +18,21 @@ public class BarcodeFragment extends Fragment {
 
     private static final String TITLE = "title";
     private static final String BARCODE = "barcode";
+    private static final String AMOUNT = "amount";
+    private static final String EXPIRES = "expires";
+
 
     private String title;
     private String barcode;
+    private String amount;
+    private String expires;
 
-    public void setArguments(String title, String identifier) {
+    public void setArguments(String title, String barcode, String amount, String expires) {
         Bundle args = new Bundle();
         args.putString(TITLE, title);
-        args.putString(BARCODE, identifier);
+        args.putString(BARCODE, barcode);
+        args.putString(AMOUNT, amount);
+        args.putString(EXPIRES, expires);
         setArguments(args);
     }
 
@@ -37,6 +44,9 @@ public class BarcodeFragment extends Fragment {
         if (args != null) {
             title = args.getString(TITLE);
             barcode = args.getString(BARCODE);
+            amount = args.getString(AMOUNT);
+            expires = args.getString(EXPIRES);
+
         }
     }
 
@@ -44,6 +54,9 @@ public class BarcodeFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle bundle) {
         Crittercism.leaveBreadcrumb("BarcodeFragment:onCreateView(): Displaying the Barcode screen.");
         View view = inflater.inflate(R.layout.barcode_fragment, container, false);
+        ((TextView) view.findViewById(R.id.reward_coupon_label)).setText(R.string.rewards_list_tabtitle);
+        ((TextView) view.findViewById(R.id.reward_coupon_value)).setText(amount);
+        ((TextView) view.findViewById(R.id.reward_coupon_expire)).setText("expires " + expires);
         ((Code128CBarcode) view.findViewById(R.id.barcode)).setText(barcode);
         ((TextView) view.findViewById(R.id.caption)).setText(barcode);
         return(view);
