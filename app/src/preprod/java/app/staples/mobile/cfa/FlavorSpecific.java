9d950a61aea81b779f61ab920f8773c22d36e194
Khandelwal, Amitav <amitav.khandelwal@gmail.com>
2015-09-08 14:11:56 -0400

    Add in MCS version

--- app/src/preprod/java/app/staples/mobile/cfa/FlavorSpecific.java
+++ app/src/preprod/java/app/staples/mobile/cfa/FlavorSpecific.java
@@ -5,5 +5,6 @@ package app.staples.mobile.cfa;
 public class FlavorSpecific {
     public static final String MCS_SERVER = "http://minternal-preprod.staples.com/";
     public static final String MCS_TAG = "CFA/preprod";
+    public static final String MCS_VERSION = "2.0";
     public static final String CRITTERCISM_ID = "5538e9208172e25e67906a3b";
 }
