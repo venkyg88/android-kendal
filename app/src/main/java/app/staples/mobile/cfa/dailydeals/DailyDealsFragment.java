63637ef53d57f99d5433e9656f97a6024448b653
Hardik Patel <hardik.patel@mindstix.com>
2015-12-08 00:29:13 +0530

    Resolved the issue of fetching data when user scrolls to bottom of daily deals list.

--- app/src/main/java/app/staples/mobile/cfa/dailydeals/DailyDealsFragment.java
+++ app/src/main/java/app/staples/mobile/cfa/dailydeals/DailyDealsFragment.java
@@ -170,8 +170,7 @@ public class DailyDealsFragment extends Fragment implements Callback<Browse> ,Da
         if (category==null)
             return(0);
 
-        complete = (category.getTotalRecords() <= offset*MAXFETCH);
-
+        complete = (category.getTotalRecords() <= offset+MAXFETCH);
         // Create adapter
         if (adapter==null) {
             adapter = new DailyDealsAdapter(getActivity());
@@ -183,10 +182,9 @@ public class DailyDealsFragment extends Fragment implements Callback<Browse> ,Da
         adapter.notifyDataSetChanged();
 
         int count = adapter.getItemCount();
-
-        if (!complete && count>=MAXFETCH)
-            adapter.setOnFetchMoreData(this, count-LOOKAHEAD);
-
+        if (!complete && count>=(MAXFETCH-1)) {
+            adapter.setOnFetchMoreData(this, count - LOOKAHEAD);
+        }
         return(count);
     }
 
