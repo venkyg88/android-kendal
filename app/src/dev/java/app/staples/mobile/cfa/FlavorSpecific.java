79cf8f661609f435ab78da23ccd8a7eab9569e12
Narayan, Savitha <Savitha.Narayan@Staples.com>
2015-12-08 11:48:27 -0500

    Configurator version changes in dev, prod, prepared and qa environments

--- app/src/dev/java/app/staples/mobile/cfa/FlavorSpecific.java
+++ app/src/dev/java/app/staples/mobile/cfa/FlavorSpecific.java
@@ -5,6 +5,6 @@ package app.staples.mobile.cfa;
 public class FlavorSpecific {
     public static final String MCS_SERVER = "http://minternal-dev.staples.com/";
     public static final String MCS_TAG = "CFA/dev";
-    public static final String MCS_VERSION = "2.0";
+    public static final String MCS_VERSION = "2.1";
     public static final String CRITTERCISM_ID = "54ff318de0697fa4496374fa";
 }
